e4d8c455ba72ae2181f3a2066cbaf0f17bb5ca85
D-W <dwright@stuy.edu>
2016-12-19 11:07:51 -0500

    push push

--- pipe_networking.c
+++ pipe_networking.c
@@ -2,11 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-int client_handshake() {
+int MESSAGE_BUFFER_SIZE = 50;
+
+int server_handshake(int *i) {
+    //1) Server creates a FIFO (Well Known Pipe)
+    //2) Server waits for a connection (open call)
+    mkfifo("atob", 0644);
+    int fd = open("atob", O_RDONLY);
+
+    //6) Server receives client’s message and removes the WKP
+    char buf[50];
+    read(fd, buf, sizeof(buf));
+    //7) Server connects to client FIFO, sending an initial acknowledgement message
+    close(fd);
+    write(i, buf, sizeof(buf));
 
 }
 
-int server_handshake() {
-    
+int client_handshake(int *i) {
+    //3) Client creates a “private” FIFO
+    //4) Client connects to server and send the private FIFO name (write)
+    mkfifo("btoa", 044);
+    int fd = open("btoa", O_WRONLY);
+    write(i, "btoa", 4);
+    char buf[50];
+    write(fd, buf, sizeof(buf));
+    //8) Client receives server’s message, removes its private FIFO
+    //9) Client sends a confirmation message to the server
+    read(i, buf, sizeof(buf));
+
+
+
 }
+
+int main() {
+    //1) Server creates a FIFO (Well Known Pipe)
+    //2) Server waits for a connection (open call)
+    mkfifo("atob", 0644);
+    return 0;
+}
+
+/*
+Basic Handshake Procedure:
+		1) Server creates a FIFO (Well Known Pipe)
+		2) Server waits for a connection (open call)
+		3) Client creates a “private” FIFO
+		4) Client connects to server and send the private FIFO name (write)
+		5) Client waits for a message from the server (open call)
+		6) Server receives client’s message and removes the WKP
+		7) Server connects to client FIFO, sending an initial acknowledgement message
+		8) Client receives server’s message, removes its private FIFO
+		9) Client sends a confirmation message to the server
+*/
--- server.c
+++ server.c
@@ -11,18 +11,15 @@ int main() {
 
   int to_client, from_client;
   char buffer[MESSAGE_BUFFER_SIZE];
-  
+
   to_client = server_handshake( &from_client );
 
   read( from_client, buffer, sizeof(buffer) );
   process( buffer );
   write( to_client, buffer, sizeof(buffer));
-  
+
   return 0;
 }
-
-
-
 
 void process( char * s ) {
 
